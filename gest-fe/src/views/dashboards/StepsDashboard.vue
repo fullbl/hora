140530f9f0260d98322b38f5a4a46e2409b45d4a
fullbl <dev@fullbl.it>
2023-07-25 20:30:54 +0200

    totals in steps dashboard

--- gest-fe/src/views/dashboards/StepsDashboard.vue
+++ gest-fe/src/views/dashboards/StepsDashboard.vue
@@ -82,6 +82,30 @@ const deliveryGroups = computed(() => {
         return x;
     }, new Map<number, Map<string, { step: string, qty: number, done: number, decigrams: number, delivery: Delivery, product: Product }>>());
 });
+
+const weekDayTotal = function (weekDay: number) {
+    let totals = 0;
+    const group = deliveryGroups.value.get(weekDay)
+    if (undefined === group) {
+        return 0
+    }
+
+    group.forEach(function (x) {
+        totals += x.qty;
+    });
+
+    return totals;
+}
+
+const weekTotal = computed(function () {
+    let totals = 0;
+    for (let i = 0; i < 6; i++) {
+        totals += weekDayTotal(i);
+    }
+
+    return totals;
+});
+
 </script>
 
 <template>
@@ -89,6 +113,7 @@ const deliveryGroups = computed(() => {
         <div class="py-3">
             <input type="number" v-model="year" placeholder="year" />
             <input type="number" v-model="week" min="1" max="53" placeholder="week" />
+            Total: {{ weekTotal }}
         </div>
         <SelectButton v-model="selectedSteps" :options="steps" optionLabel="label" optionValue="value" multiple
             aria-labelledby="multiple">
@@ -103,7 +128,8 @@ const deliveryGroups = computed(() => {
     <div class="card">
         <div class="grid">
             <div style="width:14.28%" v-for="weekDay of weekDays ">
-                <h5>{{ weekDay.label }}<br>{{ getDate(year, week, weekDay.value).toLocaleDateString() }}</h5>
+                <h5>{{ weekDay.label }} ({{ weekDayTotal(weekDay.value) }})<br>
+                    {{ getDate(year, week, weekDay.value).toLocaleDateString() }}</h5>
                 <div v-for="[name, dp] in deliveryGroups.get(weekDay.value) ">
                     <i :class="stepService.getIcon(dp.step)">{{ dp.step }}</i>
                     {{ dp.product.name }}: {{ dp.qty }} ({{ dp.qty * dp.decigrams / 10 }}g)
