423653ae49f009fc1bec5b26882829137dc5c54b
FullBL <dev@fullbl.it>
2023-12-27 19:21:15 +0100

    sentry source map

--- gest-fe/vite.config.ts
+++ gest-fe/vite.config.ts
@@ -3,11 +3,23 @@ import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     return {
-        plugins: [tsconfigPaths(), vue()],
+        build: {
+            sourcemap: true, 
+          },
+        plugins: [
+            tsconfigPaths(), 
+            vue(),
+            sentryVitePlugin({
+                org: "fullbl",
+                project: "belluno-hora-fe",
+                authToken: import.meta.env.VITE_SENTRY_AUTH_TOKEN,
+              }),
+        ],
         base: '/',
         resolve: {
             alias: {
